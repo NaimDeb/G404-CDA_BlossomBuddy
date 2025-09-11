38b213b96fc2becad53a3e8d9921cae757957a6d
Naïm D. <naimdebard02@gmail.com>
2025-09-11 13:48:45 +0200

    added login and register routes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,21 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     use HttpResponses;
 
     
-    public function register(Request $request)
+    public function register(StoreUserRequest $request)
     {
 
-        return $this->success(["Hello"]);
+        $request->validated($request->all());
 
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken("Api token of " . $user->name)->plainTextToken
+        ]);
 
     }
 
+    public function login(LoginUserRequest $request) {
+
+         if (!Auth::attempt($request->only('email', 'password'))) {
+            return $this->error('', 'Credentials does not match', 401);
+         };
+
+         $user = User::where('email', $request->email)->first();
+
+         return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('Api token of ' . $user->name)->plainTextToken
+         ]);
+
+
+    }
+ 
 }
