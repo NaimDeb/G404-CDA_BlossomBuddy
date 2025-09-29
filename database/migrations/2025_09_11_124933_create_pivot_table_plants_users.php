fe381d7f2addafcea51712412df78790427e0e2f
apprenti1 <naimdebard02@gmail.com>
2025-09-29 14:14:08 +0200

    feat : ajout de city dans table user_plant

--- database/migrations/2025_09_11_124933_create_pivot_table_plants_users.php
+++ database/migrations/2025_09_11_124933_create_pivot_table_plants_users.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->id();
             $table->foreignIdFor(Plant::class)->constrained()->cascadeOnDelete();
             $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->string('city')->nullable();
 
         });
     }
